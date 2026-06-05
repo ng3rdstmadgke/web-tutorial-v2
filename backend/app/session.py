d0281390c685cc803e682a2757a90a4ee217944c
ktamido <ng3rdstmadgke@gmail.com>
2026-06-05 18:00:31 +0900

    chapter15-start

--- backend/app/session.py
+++ backend/app/session.py
@@ -1,9 +1,9 @@
 from collections.abc import Generator
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 
 from app.config import env
-
 
 engine = create_engine(env.database_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
